b30e1dd5367f94d02d2866a45fcaddcee70ff0eb
maf-G645 <mafnitofon@mail.ru>
2022-10-09 21:29:51 +0300

    Еxample005_ConditionIfElse

---
+++ Еxample005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите имя пользователя");
+ string username = Console.ReadLine();
+ if (username == "Маша")
+    {
+        Console.WriteLine("Ура, это же МАША!");
+    }
+else
+    {
+        Console.Write("Ура, это же МАША!");
+        Console.WriteLine(username);
+    }
