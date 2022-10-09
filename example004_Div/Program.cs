3d3aeee43c405705e24b6f86a3436b1c873c8330
maf-G645 <mafnitofon@mail.ru>
2022-10-09 11:27:57 +0300

    int changed to double

---
+++ example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double numberA =12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
